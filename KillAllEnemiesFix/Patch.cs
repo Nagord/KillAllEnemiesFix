86af7e2de3665d539943dcc04204cd954cfeda86
DragonFire47 <1redyoung@gmail.com>
2021-03-06 19:37:49 -0500

    Add project files.

---
+++ KillAllEnemiesFix/Patch.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+
+namespace KillAllEnemiesFix
+{
+    [HarmonyPatch(typeof(PLMissionObjective_ReachSectorOfType), "GetNumEnemiesLeft")]
+    class Patch
+    {
+        static bool Prefix(ref int __result)
+        {
+			int num = 0;
+			foreach (PLShipInfoBase plshipInfoBase in PLEncounterManager.Instance.AllShips.Values)
+			{
+				if (plshipInfoBase != null && plshipInfoBase.TeamID == 1)
+				{
+					num++;
+				}
+			}
+			foreach (PLPawnBase plpawnBase in PLGameStatic.Instance.AllPawnBases)
+			{
+				if (plpawnBase != null && !plpawnBase.IsDead && (plpawnBase.MyPlayer == null || plpawnBase.MyPlayer.TeamID == 1))
+				{
+					num++;
+				}
+			}
+			//PulsarPluginLoader.Utilities.Logger.Info("Ran Prefix for GetNumEnemiesLeft with result " + num.ToString());
+			__result = num;
+			return false;
+		}
+    }
+	/*[HarmonyPatch(typeof(PLSpawner), "DoSpawnStatic")]
+	class Patch2
+	{
+		static void Postfix(ref bool ___HasSpawnedEnemyBeenSetup, string spawnType)
+		{
+			//PulsarPluginLoader.Utilities.Logger.Info("Ran DoSpawnStatic fix for a " + spawnType);
+			___HasSpawnedEnemyBeenSetup = true;
+		}
+	}*/
+}
---
+++ KillAllEnemiesFix/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using PulsarPluginLoader;
+
+namespace KillAllEnemiesFix
+{
+    public class Plugin : PulsarPlugin
+    {
+        public override string Version => "1.0.0";
+
+        public override string Author => "Dragon";
+
+        public override string LongDescription => "Fixes bug involving missions detecting enemies which don't exist.";
+
+        public override string Name => "KillAllEnemiesFix";
+
+        public override string HarmonyIdentifier()
+        {
+            return "Dragon.KillAllEnemiesFix";
+        }
+    }
+}
